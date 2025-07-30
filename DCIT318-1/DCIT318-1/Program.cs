775d3c583c6aca66093cb654c95d1f9782c5790c
Stan1-0 <stankofb@gmail.com>
2025-07-30 11:26:45 +0000

    Created Grade Calculator

---
+++ DCIT318-1/DCIT318-1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        Console.Write("Please enter your score (0-100): ");
+        
+        
+        string input = Console.ReadLine();
+        if (int.TryParse(input, out int grade))
+        {
+            if (grade >= 0 && grade <= 100)
+            {
+                
+                string letterGrade;
+
+                if (grade >= 90)
+                {
+                    letterGrade = "A";
+                }
+                else if (grade >= 80)
+                {
+                    letterGrade = "B";
+                }
+                else if (grade >= 70)
+                {
+                    letterGrade = "C";
+                }
+                else if (grade >= 60)
+                {
+                    letterGrade = "D";
+                }
+                else
+                {
+                    letterGrade = "F";
+                }
+
+                // Display the letter grade
+                Console.WriteLine($"The grade for your course is: {letterGrade}");
+            }
+            else
+            {
+                Console.WriteLine("Error: Grade must be between 0 and 100.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a numerical value.");
+        }
+    }
+}
